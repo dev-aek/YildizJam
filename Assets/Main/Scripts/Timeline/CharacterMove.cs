96b0bdc666836d3214a15608e84d98b3efbace31
Furkan Alliş <furkanallis1905@gmail.com>
2024-04-28 16:38:53 +0300

    mission cinematic added

---
+++ Assets/Main/Scripts/Timeline/CharacterMove.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CharacterMove : MonoBehaviour
+{
+    public float speed;
+    private void Update()
+    {
+        transform.Translate(-1*speed, 0, 0);
+
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+
+        if (other.gameObject.CompareTag("CinePoint"))
+        {
+            Debug.Log("dur");
+            speed = 0;
+        }
+        
+    }
+}
