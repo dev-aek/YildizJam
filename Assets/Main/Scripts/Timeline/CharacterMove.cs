f22f433a3ea60586b4aabe7951576076c314c4b7
Furkan Alliş <furkanallis1905@gmail.com>
2024-04-28 17:56:55 +0300

    timeline update

--- Assets/Main/Scripts/Timeline/CharacterMove.cs
+++ Assets/Main/Scripts/Timeline/CharacterMove.cs
@@ -2,25 +2,21 @@ using UnityEngine;
 
 namespace Timeline
 {
-<<<<<<< HEAD
-    public float speed;
-    public GameObject gb;
-    private void Update()
-=======
+
+
     public class CharacterMove : MonoBehaviour
->>>>>>> 3d4509f631557cdc75ec23216c01f12f4c7caa6d
+
     {
         public float speed;
         private void Update()
         {
-<<<<<<< HEAD
+
             Debug.Log("dur");
             speed = 0;
-            gb.SetActive(false);
-=======
+   
             transform.Translate(-1*speed, 0, 0);
 
->>>>>>> 3d4509f631557cdc75ec23216c01f12f4c7caa6d
+
         }
 
         private void OnTriggerEnter(Collider other)
